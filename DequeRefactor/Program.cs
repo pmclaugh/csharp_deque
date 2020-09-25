b7d9c333cd4ff8003ec0b1301aea852f019249a4
Phil McLaughlin <phil@Phils-MacBook-Pro.local>
2020-09-25 15:50:08 -0600

    had to create the project in VS2019 instead of VSC to get unit tests working as described in tutorials. some commit history lost

---
+++ DequeRefactor/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("DequeTests")]
+
+namespace DequeRefactor
+{
+#nullable enable
+    public class DLLNode
+    {
+        // node of a doubly-linked list
+        internal DLLNode? next;
+        internal DLLNode? prev;
+        internal object data;
+
+        public DLLNode(object d)
+        {
+            data = d;
+            next = null;
+            prev = null;
+        }
+
+    }
+    public class Deque
+    {
+        // Double-ended Queue (Deque)
+        internal DLLNode? head;
+        internal DLLNode? tail;
+
+        public Deque()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void init_from_empty(object o)
+        {
+            head = new DLLNode(o);
+            tail = head;
+        }
+
+        public void push(object o)
+        {
+            if (tail == null)
+                init_from_empty(o);
+            else
+            {
+                DLLNode new_tail = new DLLNode(o);
+                tail.next = new_tail;
+                new_tail.prev = tail;
+                tail = new_tail;
+            }
+        }
+
+        public void push_front(object o)
+        {
+            if (head == null)
+                init_from_empty(o);
+            else
+            {
+                DLLNode new_head = new DLLNode(o);
+                new_head.next = head;
+                head.prev = new_head;
+                head = new_head;
+            }
+        }
+
+        public object pop()
+        {
+            if (head == null)
+                return null;
+            else
+            {
+                DLLNode popped = head;
+                head = head.next;
+                if (head != null)
+                    head.prev = null;
+                else
+                    tail = null;
+                return popped.data;
+            }
+        }
+
+        public object pop_back()
+        {
+            if (tail == null)
+                return null;
+            else
+            {
+                DLLNode popped = tail;
+                tail = popped.prev;
+                if (tail != null)
+                    tail.next = null;
+                else
+                    head = null;
+                return popped.data;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DequeTests/UnitTest1.cs
@@ -0,0 +1,99 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DequeRefactor;
+
+namespace DequeTests
+{
+    [TestClass]
+    public class EmptinessTests
+    {
+        [TestMethod]
+        public void TestPushFrontOnEmpty()
+        {
+            //arrange
+            Deque dq = new Deque();
+
+            //act
+            dq.push_front("abcd");
+
+            //assert
+            Assert.AreSame(dq.head, dq.tail);
+            Assert.IsNotNull(dq.head);
+        }
+
+        [TestMethod]
+        public void TestPushOnEmpty()
+        {
+            //arrange
+            Deque dq = new Deque();
+
+            //act
+            dq.push("abcd");
+
+            //assert
+            Assert.AreSame(dq.head, dq.tail);
+            Assert.IsNotNull(dq.head);
+        }
+
+        [TestMethod]
+        public void TestPopToEmpty()
+        {
+            //arrange
+            Deque dq = new Deque();
+            dq.push("abcd");
+
+            //act
+            dq.pop();
+
+            //assert
+            Assert.AreSame(dq.head, dq.tail);
+            Assert.IsNull(dq.head);
+        }
+
+        [TestMethod]
+        public void TestPopBackToEmpty()
+        {
+            //arrange
+            Deque dq = new Deque();
+            dq.push("abcd");
+
+            //act
+            dq.pop_back();
+
+            //assert
+            Assert.AreSame(dq.head, dq.tail);
+            Assert.IsNull(dq.head);
+        }
+    }
+
+    [TestClass]
+    public class OrderingTests
+    {
+        [TestMethod]
+        public void TestQueueOrder()
+        {
+            Deque dq = new Deque();
+            for (int i = 0; i < 10; i++)
+                dq.push(i);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int j = (int)dq.pop();
+                Assert.AreEqual(i, j);
+            }
+        }
+
+        [TestMethod]
+        public void TestStackOrder()
+        {
+            Deque dq = new Deque();
+            for (int i = 0; i < 10; i++)
+                dq.push_front(i);
+
+            for (int i = 9; i >= 0; i--)
+            {
+                int j = (int)dq.pop();
+                Assert.AreEqual(i, j);
+            }
+        }
+    }
+}
